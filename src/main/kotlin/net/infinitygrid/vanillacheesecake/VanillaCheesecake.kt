8367c4c2f19644fa5bf926b675bcc2f774f44d0c
Christopher Käding <19961227+Cuube@users.noreply.github.com>
2021-06-25 19:33:58 +0200

    Register InvisibleItemFramePlacementListener to Bukkit

--- src/main/kotlin/net/infinitygrid/vanillacheesecake/VanillaCheesecake.kt
+++ src/main/kotlin/net/infinitygrid/vanillacheesecake/VanillaCheesecake.kt
@@ -1,5 +1,6 @@
 package net.infinitygrid.vanillacheesecake
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.logging.Level
 
@@ -9,11 +10,14 @@ class VanillaCheesecake : JavaPlugin() {
         lateinit var instance: VanillaCheesecake
     }
 
+    private val pluginManager = Bukkit.getPluginManager()
+
     override fun onLoad() {
         instance = this
     }
 
     override fun onEnable() {
+        pluginManager.registerEvents(InvisibleItemFrame.InvisibleItemFramePlacementListener(), this)
         logger.log(Level.INFO, "Plugin has been enabled!")
     }
 
