96c40c185f51f34d2d7d803cdfeec74fe8b88fa0
Christopher Käding <19961227+Cuube@users.noreply.github.com>
2021-07-07 19:54:34 +0200

    Add jitpack repo and maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    `maven-publish`
     java
     kotlin("jvm") version "1.5.20"
 }
@@ -10,6 +11,7 @@ repositories {
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("https://repo.pl3x.net/")
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
@@ -18,6 +20,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     compileOnly("net.pl3x.purpur", "purpur-api", "1.17-R0.1-SNAPSHOT")
     implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
 
 tasks.getByName<Test>("test") {
